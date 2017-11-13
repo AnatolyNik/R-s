ab3328ff1f3e3a7a05e5412202d0c9a68750c6a3
AnatolyNik <a_nik@hotmail.com>
2017-11-13 16:53:47 +0300

    причесал

--- G_plot.R
+++ G_plot.R
@@ -1,31 +1,34 @@
-# обработка данны эксперимента
+# Обработка данных эксперимента
+
 # загрузка библиотек 
 library("ggplot2")  # графики
+#setwd("E:/GitHub/R/Curve") # рабочая директория
 
-d_ <- read.csv("data.csv")
-head(d_)
-summary(d_)
+# чтение исходного файла
+d <- read.csv("data.csv")
+head(d)
+summary(d)
 
-t <- d_[2]
-l <- d_[3]
-l_curve <-  d_[3]
+t <- d$t
+l <- d$l
+l_curve <-  d$l_curve
 
-ggplot(data = d_, aes(x = t, y = l_curve))  + 
+ggplot(data = d, aes(x = t, y = l_curve, color = l_curve))  + stat_identity() + 
+  xlab("Время испытания, сек") +
+  ylab("Значение, Ра") +
+  scale_color_continuous("Значение") +
+  ggtitle("Зависимость давления от времени") 
+
+ggplot(data = d, aes(x = t, y = l_curve))  + 
   geom_point() + 
   geom_smooth(se = TRUE)
 #  geom_hline(yintercept = 0) +
 #  stat_qq(aes(sample = .stdresid)) +
 #  geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2)
 
-ggplot(data = d_, aes(x = t, y = l_curve, color = l_curve))  + stat_identity() + 
-  xlab("Время испытания, сек") +
-  ylab("Значение, Ра") +
-  scale_color_continuous("Значение") +
-  ggtitle("Зависимость давления от времени") 
-
-ggplot(data = d_, aes(x = t > 5, y = l_curve))  + 
+ggplot(data = d, aes(x = t > 5, y = l_curve))  + 
   geom_boxplot() 
 
-
-qplot(t, l_curve, data=d_, geom=c("point", "smooth"))
-qplot(wt, mpg, data=mtcars, geom=c("point", "smooth"))
+# примеры на встроенном датасете
+#qplot(t, l_curve, data=d, geom=c("point", "smooth"))
+#qplot(wt, mpg, data=mtcars, geom=c("point", "smooth"))
